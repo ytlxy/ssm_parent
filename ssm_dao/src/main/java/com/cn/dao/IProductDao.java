db24429f6edee94701dc5a2fc326aa78034906c1
ytlxy <a739771409@gmail.com>
2020-06-03 22:31:35 +0800

    up data

--- ssm_dao/src/main/java/com/cn/dao/IProductDao.java
+++ ssm_dao/src/main/java/com/cn/dao/IProductDao.java
@@ -10,6 +10,6 @@ public interface IProductDao {
     @Select("select * from product")
     public List<product> findAll() throws Exception;
     @Insert("insert into product(productNum,productName,cityName,departrueTime,productPrice,productDesc,productStatus)"
-            +"values(#{productNum},#{productName},#{cityName},#{departrueTime},#{productPrice},#{productDesc},#{productStatus})")
+            +"values(#{productNum},#{productName},#{cityName},#{departrueTime},#{productPrice},#{productDesc},1)")
     public void save(product product) throws Exception;
 }
